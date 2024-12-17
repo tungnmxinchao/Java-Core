f934f6f84568ab1931a569596858d8211cc4af0f
tungnm <kakal0tdz04@gmail.com>
2024-12-17 18:12:42 +0700

    queue with comparator

--- Collection/Queue/QueueDemo/src/Main.java
+++ Collection/Queue/QueueDemo/src/Main.java
@@ -1,18 +1,38 @@
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> pq = new PriorityQueue<>();
+        Queue<Integer> pq = new LinkedList<>();
+        Queue<Integer> pq2 = new PriorityQueue<>();
+        Queue<Student> students = new PriorityQueue<>();
+
         pq.add(1);
         pq.add(5);
         pq.add(3);
 
+        pq2.add(1);
+        pq2.add(5);
+        pq2.add(3);
+
+        students.add(new Student(1, "Tung"));
+        students.add(new Student(7, "Tung"));
+        students.add(new Student(5, "Tung"));
+
         while (!pq.isEmpty()) {
-            System.out.println(pq.remove());
+            System.out.println(pq.poll());
         }
 
-        System.out.println(pq);
+        System.out.println(pq2);
+
+        System.out.println(students);
+
+        while (!students.isEmpty()) {
+            System.out.println(students.poll());
+        }
+
+
 
     }
 }
---
+++ Collection/Queue/QueueDemo/src/Student.java
@@ -0,0 +1,41 @@
+public class Student implements Comparable<Student>{
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return Integer.compare(this.id, o.getId());
+    }
+}
